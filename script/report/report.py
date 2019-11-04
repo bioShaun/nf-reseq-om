57d5c6bbc5091f756affe81bd0a4a5b501a74dc3
Bioshaun <ricekent@163.com>
2019-11-04 04:45:29 -0500

    new report template

--- script/report/report.py
+++ script/report/report.py
@@ -1,9 +1,13 @@
-import jinja2
-import fire
-from pathlib import Path, PurePath
 import os
 import glob
 import sys
+import jinja2
+import fire
+import pandas as pd
+from io import StringIO
+from pathlib import Path, PurePath
+
+pd.set_option('precision', 3)
 
 script_dir, _ = os.path.split(os.path.abspath(__file__))
 
@@ -15,20 +19,42 @@ template = env.get_template('index.html')
 # code that fills in display_dictionary with the values to send to the template
 
 
-def table2dict(table_file, name, sep='\t'):
+def format_map_df(map_df):
+    t_map_df = map_df.T
+    t_map_df.columns = [each.rstrip(':') for each in t_map_df.columns]
+    int_df = t_map_df.iloc[:, [0, 1, 4, 3]]
+    float_df = t_map_df.iloc[:, -3:]
+    int_df = int_df.astype('int')
+    int_df = int_df.applymap(lambda x: f'{x:,}')
+    clean_df = pd.concat([int_df, float_df], axis=1)
+    clean_df.index.name = 'Item'
+    return clean_df
+
+
+def format_reads_df(reads_df):
+    int_df = reads_df.iloc[:, 0:4]
+    float_df = reads_df.iloc[:, -5:]
+    float_df = float_df.applymap(lambda x: f'{x:.3f}')
+    int_df = int_df.astype('int').applymap(lambda x: f'{x:,}')
+    clean_df = pd.concat([int_df, float_df], axis=1)
+    clean_df.index.name = 'Item'
+    return clean_df
+
+
+def table2dict(table_file, name, sep='\t', format_func=None):
     table_dict = dict()
-    if table_file.exists():
-        with open(table_file) as table_inf:
-            for n, eachline in enumerate(table_inf):
-                eachline_inf = eachline.split(sep)
-                if n == 0:
-                    label = '{}_header'.format(name)
-                    table_dict[label] = eachline_inf
-                else:
-                    label = '{}_body'.format(name)
-                    table_dict.setdefault(label, []).append(eachline_inf)
-    if 'snp' in name:
-        table_dict['snp'] = True
+    if table_file.is_file():
+        table_df = pd.read_csv(table_file, sep=sep, index_col=0)
+        if format_func is not None:
+            table_df = format_func(table_df)
+        table_df.sort_index(inplace=True)
+        table_df = table_df.reset_index()
+        for idx_i in table_df.index:
+            table_dict.setdefault(
+                f'{name}_body', []).append(list(table_df.loc[idx_i]))
+        table_dict[f'{name}_header'] = list(table_df.columns)
+        if 'snp' in name:
+            table_dict['snp'] = True
     return table_dict
 
 
@@ -47,6 +73,15 @@ def plot2report(plot_path, outpath, plot_flag, plot_name=None):
     return plot_dict
 
 
+def plotlist2report(plot_list, outpath, plot_flag):
+    plot_dict = dict()
+    if plot_list:
+        for plot in plot_list:
+            os.system(f'cp {plot} {outpath}')
+        plot_dict[plot_flag] = [PurePath(each).name for each in plot_list]
+    return plot_dict
+
+
 def exom_report(result_dir, proj_name, report_dir=None):
     result_dir = Path(result_dir)
     if report_dir is None:
@@ -61,12 +96,13 @@ def exom_report(result_dir, proj_name, report_dir=None):
     # add fastqc table
     qc_table = result_dir / 'reads_qc/data.summary.csv'
     display_dictionary.update(
-        table2dict(qc_table, 'seq', sep=','))
+        table2dict(qc_table, 'seq', sep=',', format_func=format_reads_df))
 
     # add aligment table
     align_table = result_dir / 'alignment/mapping.summary.csv'
     display_dictionary.update(
-        table2dict(align_table, 'align', sep=','))
+        table2dict(
+            align_table, 'align', sep=',', format_func=format_map_df))
 
     # snp stats
     # summary
@@ -123,47 +159,48 @@ def exom_report(result_dir, proj_name, report_dir=None):
         for dir_i in variant_summary_plot_dir.iterdir():
             if dir_i.is_dir():
                 example_sample = dir_i.name
-        varType_plot = result_dir / \
-            f'plot/variants/{example_sample}/{example_sample}_varType.png'
-        display_dictionary.update(
-            plot2report(varType_plot, report_plot_path,
-                        'variant_type', 'varType'))
 
-        varRegion_plot = result_dir / \
-            f'plot/variants/{example_sample}/{example_sample}_varRegion.png'
+        varType_plot = glob.glob(f'{result_dir}/plot/variants/*/*_varType.png')
         display_dictionary.update(
-            plot2report(varRegion_plot, report_plot_path,
-                        'variant_region', 'varRegion'))
+            plotlist2report(varType_plot, report_plot_path,
+                            'varType_plots'))
 
-        varEffects_high_plot = result_dir / \
-            f'plot/variants/{example_sample}/{example_sample}_varEffects-HIGH.png'
+        varRegion_plot = glob.glob(
+            f'{result_dir}/plot/variants/*/*_varRegion.png')
         display_dictionary.update(
-            plot2report(varEffects_high_plot, report_plot_path,
-                        'variant_effect_high', 'varEffects-HIGH'))
+            plotlist2report(varRegion_plot, report_plot_path,
+                            'varRegion_plots'))
 
-        varEffects_moderate_plot = result_dir / \
-            f'plot/variants/{example_sample}/{example_sample}_varEffects-MODERATE.png'
+        varImpact_plot = glob.glob(
+            f'{result_dir}/plot/variants/*/*_varImpact.png')
         display_dictionary.update(
-            plot2report(varEffects_moderate_plot,
-                        report_plot_path,
-                        'variant_effect_moderate', 'varEffects-MODERATE'))
+            plotlist2report(varImpact_plot,
+                            report_plot_path, 'varImpact_plots'))
+        # varEffects_high_plot = result_dir / \
+        #     f'plot/variants/{example_sample}/{example_sample}_varEffects-HIGH.png'
+        # display_dictionary.update(
+        #     plot2report(varEffects_high_plot, report_plot_path,
+        #                 'variant_effect_high', 'varEffects-HIGH'))
 
-        varEffects_low_plot = result_dir / \
-            f'plot/variants/{example_sample}/{example_sample}_varEffects-LOW.png'
-        display_dictionary.update(
-            plot2report(varEffects_low_plot, report_plot_path,
-                        'variant_effect_low', 'varEffects-LOW'))
+        # varEffects_moderate_plot = result_dir / \
+        #     f'plot/variants/{example_sample}/{example_sample}_varEffects-MODERATE.png'
+        # display_dictionary.update(
+        #     plot2report(varEffects_moderate_plot,
+        #                 report_plot_path,
+        #                 'variant_effect_moderate', 'varEffects-MODERATE'))
 
-        varEffects_modifier_plot = result_dir / \
-            f'plot/variants/{example_sample}/{example_sample}_varEffects-MODIFIER.png'
-        display_dictionary.update(
-            plot2report(varEffects_modifier_plot,
-                        report_plot_path,
-                        'variant_effect_modifier', 'varEffects-MODIFIER'))
-        varImpact_plot = result_dir / \
-            f'plot/variants/{example_sample}/{example_sample}_varImpact.png'
-        display_dictionary.update(plot2report(varImpact_plot,
-                                              report_plot_path, 'variant_impact', 'varImpact'))
+        # varEffects_low_plot = result_dir / \
+        #     f'plot/variants/{example_sample}/{example_sample}_varEffects-LOW.png'
+        # display_dictionary.update(
+        #     plot2report(varEffects_low_plot, report_plot_path,
+        #                 'variant_effect_low', 'varEffects-LOW'))
+
+        # varEffects_modifier_plot = result_dir / \
+        #     f'plot/variants/{example_sample}/{example_sample}_varEffects-MODIFIER.png'
+        # display_dictionary.update(
+        #     plot2report(varEffects_modifier_plot,
+        #                 report_plot_path,
+        #                 'variant_effect_modifier', 'varEffects-MODIFIER'))
 
     # deltaSNP_plot = result_dir / 'mapping/*deltaSNP.png'
     # Gprime_plot = result_dir / 'mapping/*Gprime.png'
@@ -177,7 +214,7 @@ def exom_report(result_dir, proj_name, report_dir=None):
     report_html = report_dir / 'index.html'
     with open(report_html, 'w') as out_inf:
         out_inf.write(display_html)
-    os.system(f'tar -zcf {report_dir}.tar.gz {report_dir}')
+    os.system(f'tar -zcf {report_dir}.tar.gz -C {report_dir.parent} {report_dir.name}')
 
 
 if __name__ == '__main__':
